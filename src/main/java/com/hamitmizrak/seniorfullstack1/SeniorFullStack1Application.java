9088fc197170e5a94e8df6ffe76667f6d5449191
Hamit Mizrak <hamitmizrak@gmail.com>
2024-06-29 13:33:41 +0300

    Project initial Data

---
+++ src/main/java/com/hamitmizrak/seniorfullstack1/SeniorFullStack1Application.java
@@ -0,0 +1,73 @@
+package com.hamitmizrak.seniorfullstack1;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.util.TimeZone;
+
+
+// Mongo aktif etmek ici
+// @EnableMongoRepositories
+
+// Aspect aktif etmek icin
+// @EnableAspectJAutoProxy(proxyTargetClass = true)
+
+// Asenkron açmak icin
+// @EnableAsync
+
+// SCAN
+// @EntityScan(basePackages = "com.hamitmizrak.data.entity") //Entity bulamadığı zaman
+// @EnableJpaRepositories(basePackages = "com.hamitmizrak.data.repository") //Repository bulamadığı zaman
+
+// Spring Cache aktif etmek gerekiyor.
+// @EnableCaching
+
+// Auditing Aktif etmek
+// @EnableJpaAuditing(auditorAwareRef = "auditorAwareBeanMethod")
+
+// Spring Security: Şimdilik dahil etme ancak Spring security için gerekli kütüphaneleri dahil
+@SpringBootApplication(exclude = {
+        //SecurityAutoConfiguration.class,
+        //org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        //org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+// @SpringBootApplication
+//public class FullStackDeveloper12Application {
+
+// Apache Tomcat için: extends SpringBootServletInitializer
+
+
+//@SpringBootApplication
+public class SeniorFullStack1Application {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+    }
+
+    public static void main(String[] args) {
+
+        // devtools active isActive
+        // System.setProperty("spring.devtools.restart.enabled","true");
+
+        // PORT Ayarlamak
+        /*
+        SpringApplication app = new SpringApplication(TurgutUniversitySpringAllInOneApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
+         */
+
+        // JOptional pane aktif etmek
+        System.setProperty("java.awt.headless", "false");
+
+        // Main
+        SpringApplication.run(SeniorFullStack1Application.class, args);
+    }
+
+
+
+}
