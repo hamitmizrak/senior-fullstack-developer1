4c7b248e41b7c2ea44173ddf8514647371aba938
Hamit_Mizrak <hamitmizrak@gmail.com>
2024-07-06 12:27:18 +0300

    Blog categories Services

---
+++ src/main/java/com/hamitmizrak/seniorfullstack1/business/services/IBlogCategoriesServices.java
@@ -0,0 +1,38 @@
+package com.hamitmizrak.seniorfullstack1.business.services;
+
+import java.util.List;
+
+// D: Dto
+// E: Entity
+public interface IBlogCategoriesServices<D,E> {
+
+    // MODEL MAPPER
+    public D entityToDto(E e);
+    public E dtoToEntity(D d);
+
+    /////////////////////////////////////////////////////////////
+    // SPEED DATA
+    public String blogCategorySpeedData(Integer data);
+
+    // DELETE ALL
+    public String blogCategoryDeleteAllData();
+
+    ////////////////////////////////////////////////////////////
+
+    // CRUD
+    // CREATE
+    public D blogCategoryServiceCreate(D d);
+
+    // LIST
+    public List<D> blogCategoryServiceList();
+
+    // FIND
+    public D blogCategoryServiceFindById(Long id);
+
+    // UPDATE
+    public D blogCategoryServiceUpdateById(Long id,D d);
+
+    // DELETE
+    public D blogCategoryServiceDeleteById(Long id);
+
+} //end interface
---
+++ src/main/java/com/hamitmizrak/seniorfullstack1/business/services/impl/BlogCategoryServicesImpl.java
@@ -0,0 +1,8 @@
+package com.hamitmizrak.seniorfullstack1.business.services.impl;
+
+import com.hamitmizrak.seniorfullstack1.business.dto.BlogCategoryDto;
+import com.hamitmizrak.seniorfullstack1.business.services.IBlogCategoriesServices;
+import com.hamitmizrak.seniorfullstack1.data.entity.BlogCategoryEntity;
+
+public class BlogCategoryServicesImpl implements IBlogCategoriesServices<BlogCategoryDto, BlogCategoryEntity> {
+}
