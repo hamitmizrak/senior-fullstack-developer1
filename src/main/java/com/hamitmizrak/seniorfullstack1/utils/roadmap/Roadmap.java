131adcb9f549c8c313ac2b842a77ac42a4ab3bcd
Hamit Mızrak <hamitmizrak@gmail.com>
2024-07-20 09:36:21 +0300

    roadmap

---
+++ src/main/java/com/hamitmizrak/seniorfullstack1/utils/roadmap/Roadmap.java
@@ -0,0 +1,15 @@
+package com.hamitmizrak.seniorfullstack1.utils.roadmap;
+
+public class Roadmap {
+    // BlogCategory
+    // Blog
+    // Swagger-UI
+    // Postman
+    // Database: H2DB
+    // Database: Mysql
+    // Database: Postgresql
+    // Dockerize: H2DB, Mysql, Postgresql
+    // JMeter Stress Testi
+    // SonarQube - Static Code Analiz (Dockerize)
+    // React JS
+}
