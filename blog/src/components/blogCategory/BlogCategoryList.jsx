184103fbcd36ab7169ca3ad55c40bfae58794924
Hamit Mızrak <hamitmizrak@gmail.com>
2024-07-29 17:57:32 +0300

    BlogCategoryList

--- blog/src/components/blogCategory/BlogCategoryList.jsx
+++ blog/src/components/blogCategory/BlogCategoryList.jsx
@@ -1,10 +1,35 @@
 // rfce
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-function BlogCategoryList() {
-  return (
-    <div>BlogCategoryList</div>
-  )
-}
+//REDIRECT
+import { useNavigate } from "react-router-dom";
 
-export default BlogCategoryList
+// FUNCTION
+function BlogCategoryList({ t, i18n, props }) {
+  // REDIRECT
+  let navigate = useNavigate();
+
+  // STATE
+  const [blogCategoryApiImplListData, setBlogCategoryApiImplListData] =
+    useState([]);
+
+  // EFFECT
+  useEffect(() => {
+    // fetchBlogCategoryApiImplListData();
+  }, []);
+
+  // API CALLS
+  const fetchBlogCategoryApiImplListData = () => {}; //end fetchBlogCategoryApiImplListData
+  ///////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+
+  // FUNCTION
+
+  ///////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+  // return
+  return <div>BlogCategoryList</div>; //end return
+} //end BlogCategoryList
+
+// EXPORT
+export default BlogCategoryList;
